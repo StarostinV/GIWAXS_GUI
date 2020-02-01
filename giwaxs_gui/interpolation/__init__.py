0ddb8585ebc5a76e1ddac6ca791eecb93c29d5d6
StarostinV <vladimir.starostin@uni-tuebingen.de>
2020-02-01 17:06:00 +0100

    deleted old interpolation package and its binaries

--- giwaxs_gui/interpolation/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from .polar_coordinates import convert_image, get_radial_profile
-
--- giwaxs_gui/interpolation/boxInterpolation.h
+++
@@ -1,6 +0,0 @@
-extern "C" double *boxInterpolation(const double *inten,
-                                    const double *qx, const double *qy, int ndat,
-                                    double xs, double xdel, int nx, double xhw,
-                                    double ys, double ydel, int ny, double yhw);
-
-extern "C" void deleteResult(double * result);
