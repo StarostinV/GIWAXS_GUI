50225892e14aa9ef9fabcafb8df26d1a46c3b34d
StarostinV <vladimir.starostin@uni-tuebingen.de>
2020-02-04 17:17:10 +0100

    added movable arguments with False by default

--- giwaxs_gui/gui/basic_widgets/toolbars.py
+++ giwaxs_gui/gui/basic_widgets/toolbars.py
@@ -2,14 +2,16 @@ from PyQt5.QtWidgets import QToolBar
 
 
 class ToolBar(QToolBar):
-    def __init__(self, name: str, parent=None, color: str = None, disable_hide: bool = True):
+    def __init__(self, name: str, parent=None, color: str = None, *,
+                 disable_hide: bool = True, movable: bool = False):
         super().__init__(name, parent)
         if color:
             self.setStyleSheet(f'background-color: {color};')
         if disable_hide:
             self.toggleViewAction().setEnabled(False)
+        self.setMovable(movable)
 
 
 class BlackToolBar(ToolBar):
-    def __init__(self, name: str, parent=None, disable_hide: bool = True):
-        super().__init__(name, parent, 'black', disable_hide)
+    def __init__(self, name: str, parent=None, **kwargs):
+        super().__init__(name, parent, 'black', **kwargs)
